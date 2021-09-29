23c925a20d82f095c89cba6d92611e5277ce428c
kurosawa1109 <88430509+kurosawa1109@users.noreply.github.com>
2021-09-29 14:48:22 +0900

    ページ作成

---
+++ app/controllers/resutorans_controller.rb
@@ -0,0 +1,6 @@
+class ResutoransController < ApplicationController
+
+  def index
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root to: "tops#index"
   resources :tops, only: :index
   resources :posts,only: [:index, :new, :create]
+  resources :resutorans, only: :index
 end
