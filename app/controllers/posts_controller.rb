d841609c481ddb0910bcf52794fed63701321e3a
kurosawa1109 <88430509+kurosawa1109@users.noreply.github.com>
2021-10-03 13:36:35 +0900

    修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, only: [:index, :show]
   before_action :move_to_index, only: :destroy
 
   def index
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,5 @@
 class RestaurantsController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :show]
 
   def index
   end
